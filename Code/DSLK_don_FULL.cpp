dd2591be0092370a535bf62396d05346a26d7533
knguyen <nguyendinhkhanhnguyen1@gmail.com>
2024-05-20 19:26:39 +0700

    cpp

--- Code/DSLK_don_FULL.cpp
+++ Code/DSLK_don_FULL.cpp
@@ -12,8 +12,8 @@ using namespace std;
 #define se second
 #define ll long long
 long long t,n;
-// #define m 1000000007
-// #define e 0.000000001
+//#define m 1000000007
+//#define e 0.000000001
 // F5 to run code
 void Solve();
 void Task() {
@@ -27,130 +27,147 @@ void Task() {
     Solve();
     cerr << "Time: " << (clock() - begin + 1.0) / CLOCKS_PER_SEC << "s";
 }
-//Node
 struct Node{
     int data;
-    Node* next;
+    Node * next;
 };
-//define con trỏ Node là node
-typedef struct Node * node;
-//tạo node mới
-node crnode(int x){
-    node p = new Node;
-    p->data = x;
-    p->next = NULL;
-    return p;
+typedef Node * node;
+node crnode(int n){
+    node newnode= new Node;
+    newnode->data=n;
+    newnode->next=NULL;
+    return newnode;
 }
-//dem phần tử
-size_t Size(node head){
-    size_t n = 0;
-    while(head != NULL){
-        n++;
-        head=head->next;
-    }
-    return n;
+void insF(node &head,int n){
+    node newnode=crnode(n);
+    newnode->next=head;
+    head=newnode;
 }
-//chèn đầu
-void insF(node &head, int x){
-    node tmp=crnode(x);
+void insL(node &head,int n){
+    node newnode=crnode(n);
     if(head==NULL){
-        head=tmp;
-    }else{
-        tmp->next = head;
-        head=tmp;
-    }
-}
-//chèn cuối
-void insL(node &head, int x){
-    node tmp=crnode(x);
-    if(head==NULL){
-        head=tmp;
+        head=newnode;
     }else{
         node p=head;
-        while(p->next){
+        while (p->next)
+        {
             p=p->next;
         }
-        p->next=tmp;
+        p->next=newnode;
     }
 }
-//chèn trước p
-void insP(node &head, int x,int p){
-    int n=Size(head);
-    if(p<=0||p>=n+1){
-        cout<<"Loi"<<endl;
-        return;
-    }
-    if(n==1){
-        insF(head,x);
-    }
-    else if(n==p+1){
-        insL(head,x);
-    }
-    else{
-        node ct=head;
-        for(int i=1;i<p-1;i++){
-            ct=ct->next;
-        }
-        node tmp=crnode(x);
-        tmp->next=ct->next;
-        ct->next=tmp;
-    }
-}
-//xóa đầu
-void DeleteF(node &head){
-    if(head==NULL){
-        return;
-    }
-    else{
+int dem(node head){
+    int c=0;
+    while(head!=NULL){
+        c++;
         head=head->next;
     }
+    return c;
 }
-//Xóa cuối
-void DeleteL(node &head){
-    if(head==NULL){
-        return;
-    }else{
-        node truoc=NULL,sau=head;
-        while(sau->next){
-            truoc=sau;
-            sau=sau->next;
-        }
-        if(truoc==NULL){
-            head=NULL;
-        }
-        else{
-            truoc->next=NULL;
-        }
-    }
-}
-//xóa vị trí p
-void DeleteP(node &head,int p){
-    if(p<=0||p>Size(head)){
-        return;
-    }
-    node truoc=NULL,sau=head;
-    for(int i=0;i<p-1;i++){
-        truoc=sau;
-        sau=sau->next;
-    }
-    if(truoc==NULL){
+void duyet(node head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
         head=head->next;
-    }else{
-        truoc->next=sau->next;
+    }
+    cout<<endl;
+}
+void insP(node &head,int k,int n){
+    node newnode=crnode(n);
+    if(k<1||k>dem(head)+1){
+        return;
+    }else if(k==1){
+        insF(head,n);
+    }else {
+        node p=head;
+        for(int i=1;i<=k-2;i++){
+            p=p->next;
+        }
+        newnode->next=p->next;
+        p->next=newnode;
     }
 }
-//in ra
-void in(node head){
-    while(head != NULL){
-        cout << head->data << " ";
-        head = head->next;
-    }
-    cout << endl;
+void deF(node &head){
+    node temp=head;
+    head=head->next;
+    delete temp;
 }
-void Solve(){
-    //khoi tao node
+void deL(node &head) {
+    node p = head;
+    node tr = NULL;
+    while (p->next) {
+        tr = p;
+        p = p->next;
+    }
+    tr->next = NULL;
+    delete p;
+}
+void deP(node &head, int x) {
+    node p = head;
+    if (x == 1) {
+        deF(head);
+        return;
+    }
+    for (int i = 1; i <= x - 2; i++) {
+        p = p->next;
+    }
+    node temp = p->next;
+    p->next = temp->next;
+}
+void deK(node &head,int k){
+    if(head->data==k){
+        deF(head);
+        return;
+    }
+     node p = head;
+    while (p->next!=NULL) {
+        if (p->next->data == k) {
+            node temp = p->next;
+            p->next = temp->next;
+            delete temp;
+        } else {
+            p = p->next;
+        }
+    }
+    
+}
+void Solve() {
+    int n;
     node head=NULL;
- 
+    while(cin>>n){
+        if(n==1){
+            int x;
+            cin>>x;
+            insF(head,x);
+        }
+        if(n==2){
+            int x;
+            cin>>x;
+            insL(head,x);
+        }
+        if(n==3){
+            int x,k;
+            cin>>k>>x;
+            insP(head,k,x);
+        }
+        if(n==4){
+            deF(head);
+        }
+        if(n==5){
+            deL(head);
+        }
+        if(n==6){
+            int x; cin>>x;
+            deP(head,x);
+        }
+        if(n==8){
+            int k;
+            cin>>k;
+            deK(head,k);
+        }
+        if(n==7){
+            duyet(head);
+        }
+    }   
 }
 int main() {
     Task();
