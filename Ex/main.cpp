ab0d750db2b928bfab8706328a5722c41b839bf1
knguyen <nguyendinhkhanhnguyen1@gmail.com>
2024-05-21 15:19:28 +0700

    cpp

--- Ex/main.cpp
+++ Ex/main.cpp
@@ -1,174 +1,173 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define name "test"
-#define fr(i,a,b) for (auto i = a ; i < b ;i++)
-#define FR(i,a,b) for (auto i = a ; i < b ;i--)
-#define fx(a) for (auto x:a)
-#define w(n) while(n--)
-#define gl(s) getline(cin,s)
-#define c(n) cin>>n;
+#define namebt "test"
+#define fr(i, a, b) for (auto i = a; i < b; i++)
+#define fx(a) for (auto &x : a)
 #define pb push_back
-#define fi first
-#define se second
 #define ll long long
-long long t,n;
-//#define m 1000000007
-//#define e 0.000000001
-// F5 to run code
+long long t, n;
+// #define m 1000000007
+// #define e 0.000000001
 void Solve();
-void Task() {
+void Task()
+{
     clock_t begin = clock();
-    ios_base :: sync_with_stdio(false); cin.tie(0); cout.tie(0);
-    if (fopen(name ".inp", "r"))
+    ios_base ::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    if (fopen(namebt ".inp", "r"))
     {
-        freopen(name ".inp", "r", stdin);
-        freopen(name ".out", "w", stdout);
+        freopen(namebt ".inp", "r", stdin);
+        freopen(namebt ".out", "w", stdout);
     }
     Solve();
     cerr << "Time: " << (clock() - begin + 1.0) / CLOCKS_PER_SEC << "s";
 }
-struct Node{
+struct Node
+{
     int data;
-    Node * next;
+    struct Node *next;
 };
-typedef Node * node;
-node crnode(int n){
-    node newnode= new Node;
-    newnode->data=n;
-    newnode->next=NULL;
-    return newnode;
+typedef Node *node;
+node cr(int n)
+{
+    node head = new Node;
+    head->data = n;
+    head->next = NULL;
+    return head;
 }
-void insF(node &head,int n){
-    node newnode=crnode(n);
-    newnode->next=head;
-    head=newnode;
-}
-void insL(node &head,int n){
-    node newnode=crnode(n);
-    if(head==NULL){
-        head=newnode;
-    }else{
-        node p=head;
-        while (p->next)
-        {
-            p=p->next;
-        }
-        p->next=newnode;
-    }
-}
-int dem(node head){
-    int c=0;
-    while(head!=NULL){
+size_t dem(node head)
+{
+    size_t c = 0;
+    while (head != NULL)
+    {
         c++;
-        head=head->next;
+        head = head->next;
     }
     return c;
 }
-void duyet(node head){
-    while(head!=NULL){
-        cout<<head->data<<" ";
-        head=head->next;
+void insL(node &head, int n)
+{
+    node newnode = cr(n);
+    if (head == NULL)
+    {
+        head = newnode;
     }
-    cout<<endl;
-}
-void insP(node &head,int k,int n){
-    node newnode=crnode(n);
-    if(k<1||k>dem(head)+1){
-        return;
-    }else if(k==1){
-        insF(head,n);
-    }else {
-        node p=head;
-        for(int i=1;i<=k-2;i++){
-            p=p->next;
+    else
+    {
+        node p = head;
+        while (p->next != NULL)
+        {
+            p = p->next;
         }
-        newnode->next=p->next;
-        p->next=newnode;
+        p->next = newnode;
     }
 }
-void deF(node &head){
-    node temp=head;
-    head=head->next;
+void insP(node &head, int n, int p)
+{
+    node newnode = cr(n);
+    size_t k=dem(head);
+    if (p < 1)
+    {
+        return;
+    }
+    else if (p == 1)
+    {
+        newnode->next = head;
+        head = newnode;
+    }
+    else if (p > k)
+    {
+        insL(head, n);
+    }
+    else
+    {
+        node x = head;
+        for (int i = 1; i <= p - 2; i++)
+        {
+            x = x->next;
+        }
+        newnode->next = x->next;
+        x->next = newnode;
+    }
+}
+void deF(node &head)
+{
+    node temp = head;
+    head = head->next;
     delete temp;
 }
-void deL(node &head) {
+void deP(node &head, int x)
+{
     node p = head;
-    node tr = NULL;
-    while (p->next) {
-        tr = p;
-        p = p->next;
-    }
-    tr->next = NULL;
-    delete p;
-}
-void deP(node &head, int x) {
-    node p = head;
-    if (x == 1) {
+    if (x == 1)
+    {
         deF(head);
         return;
     }
-    for (int i = 1; i <= x - 2; i++) {
+    for (int i = 1; i <= x - 2; i++)
+    {
         p = p->next;
     }
     node temp = p->next;
     p->next = temp->next;
 }
-void deK(node &head,int k){
-    if(head->data==k){
-        deF(head);
-        return;
+float Average(node head)
+{
+    float sum = 0, cnt = dem(head);
+    while (head != NULL)
+    {
+        sum += (float)head->data;
+        head = head->next;
     }
-     node p = head;
-    while (p->next!=NULL) {
-        if (p->next->data == k) {
-            node temp = p->next;
-            p->next = temp->next;
-            delete temp;
-        } else {
-            p = p->next;
-        }
-    }
-    
+    return sum / (float)cnt;
 }
-void Solve() {
+void duyet(node head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+void Solve()
+{
+    node head = NULL;
     int n;
-    node head=NULL;
-    while(cin>>n){
-        if(n==1){
-            int x;
-            cin>>x;
-            insF(head,x);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int data;
+        cin >> data;
+        insL(head, data);
+    }
+    char temp;
+    while (cin >> temp)
+    {
+        if (temp == 'i')
+        {
+            int x, p;
+            cin >> x >> p;
+            insP(head, x, p);
         }
-        if(n==2){
-            int x;
-            cin>>x;
-            insL(head,x);
+        else if (temp == 'd')
+        {
+            int p;
+            cin >> p;
+            deP(head, p);
         }
-        if(n==3){
-            int x,k;
-            cin>>k>>x;
-            insP(head,k,x);
+        else if (temp == 'a')
+        {
+            cout << fixed << setprecision(4) << (float)Average(head) << '\n';
         }
-        if(n==4){
-            deF(head);
+        else
+        {
+            break;
         }
-        if(n==5){
-            deL(head);
-        }
-        if(n==6){
-            int x; cin>>x;
-            deP(head,x);
-        }
-        if(n==8){
-            int k;
-            cin>>k;
-            deK(head,k);
-        }
-        if(n==7){
-            duyet(head);
-        }
-    }   
+    }
 }
-int main() {
+int main()
+{
     Task();
 }
