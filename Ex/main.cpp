172ff28d56879b47591e90ff419061caeaecafe9
KNguyen1411b <nguyendinhkhanhnguyen1@gmail.com>
2024-06-06 17:12:11 +0700

    a

--- Ex/main.cpp
+++ Ex/main.cpp
@@ -1,35 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define filename "test"
+#define name "test"
 #define fr(i, a, b) for (auto i = a; i < b; i++)
+#define FR(i, a, b) for (auto i = a; i < b; i--)
 #define fx(a) for (auto x : a)
+#define w(n) while (n--)
+#define gl(s) getline(cin, s)
+#define c(n) cin >> n;
 #define pb push_back
+#define fi first
+#define se second
 #define ll long long
-long long t, n;
 // #define m 1000000007
 // #define e 0.000000001
+long long t, n;
+// F5 to run code
 void Solve();
 void Task(){
-    #ifndef ONLINE_JUDGE
     clock_t begin = clock();
     ios_base ::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    if (fopen(filename ".inp", "r"))
+    if (fopen(name ".inp", "r"))
     {
-        freopen(filename ".inp", "r", stdin);
-        freopen(filename ".out", "w", stdout);
+        freopen(name ".inp", "r", stdin);
+        freopen(name ".out", "w", stdout);
     }
-    #endif
     Solve();
-    #ifndef ONLINE_JUDGE
     cerr << "Time: " << (clock() - begin + 1.0) / CLOCKS_PER_SEC << "s";
-    #endif
 }
 void Solve(){
-    
+
 }
 int main(){
     Task();
 }
-
