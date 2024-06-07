b0733a580613fc419f42aceae7ae71843768f808
KNguyen1411b <nguyendinhkhanhnguyen1@gmail.com>
2024-06-07 09:32:11 +0700

    a

--- Ex/main.cpp
+++ Ex/main.cpp
@@ -29,6 +29,68 @@ void Task(){
     Solve();
     cerr << "Time: " << (clock() - begin + 1.0) / CLOCKS_PER_SEC << "s";
 }
+int binarySearch(int a[],int n,int x){
+    int left = 0;
+    int right = n-1;
+    while(left<=right){
+        int mid = (left+right)/2;
+        if(a[mid]==x){
+            return mid;
+        }
+        else if(a[mid]<x){
+            left = mid+1;
+        }
+        else{
+            right = mid-1;
+        }
+    }
+    return -1;
+}
+double DetMatrix(double matrix[][100], int n) {
+    if (n == 1) {
+        return matrix[0][0];
+    }
+    if (n == 2) {
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    }
+    double det = 0;
+    for (int i = 0; i < n; i++) {
+        double subMatrix[100][100];
+        for (int j = 1; j < n; j++) {
+            for (int k = 0; k < i; k++) {
+                subMatrix[j - 1][k] = matrix[j][k];
+            }
+            for (int k = i + 1; k < n; k++) {
+                subMatrix[j - 1][k - 1] = matrix[j][k];
+            }
+        }
+        det += (i % 2 == 0 ? 1 : -1) * matrix[0][i] * DetMatrix(subMatrix, n - 1);
+    }
+    return det;
+}
+int ss(string s1,string s2) {
+    string kq;
+    for (size_t i = 0; i < s1.length(); i++) {
+        kq += (s1[i] != s2[i]) ? '1' : '0';
+    }
+    return bitset<64>(kq).count();
+}
+void ssx(){
+    int n,t;
+    cin>>t>>n;
+    vector<string>luus(n);
+    fr(i,0,n) cin>>luus[i];
+    fr(i,0,n){
+        int ma=0;
+        fr(j,0,n){
+            if(i!=j){
+                int a=ss(luus[i],luus[j]);
+                ma=max(ma,a);
+            }
+        }
+        cout<<ma<<endl;
+    }
+}
 void Solve(){
     
 }
